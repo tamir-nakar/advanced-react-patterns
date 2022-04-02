335c26880d0d7e0e025f82e9f5687efb43cb937f
tamir-nakar <nakar.tamir@gmail.com>
2022-04-02 13:34:24 +0300

    1-2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,39 +4,58 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-function Toggle() {
+function Toggle(props) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 replace this with a call to React.Children.map and map each child in
-  // props.children to a clone of that child with the props they need using
-  // React.cloneElement.
-  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
-  // 📜 https://reactjs.org/docs/react-api.html#reactchildren
-  // 📜 https://reactjs.org/docs/react-api.html#cloneelement
-  return <Switch on={on} onClick={toggle} />
+  return React.Children.map(props.children, child => {
+    if(allowedTypes.includes(child.type)){
+
+      return React.cloneElement(child, {on, toggle })
+    }else{
+      return child
+    }
+  } )
+
 }
 
 // 🐨 Flesh out each of these components
 
 // Accepts `on` and `children` props and returns `children` if `on` is true
-const ToggleOn = () => null
+const ToggleOn = ({on, children}) => {
+  if(on){
+    return children
+  }else{
+    return null;
+  }
+}
 
 // Accepts `on` and `children` props and returns `children` if `on` is false
-const ToggleOff = () => null
+const ToggleOff = ({on, children}) => {
+
+  if(!on){
+    return children
+  }else{
+    return null
+  }
+}
 
 // Accepts `on` and `toggle` props and returns the <Switch /> with those props.
-const ToggleButton = () => null
+const ToggleButton = ({on, toggle}) => {
+  return <Switch on={on} onClick={toggle} />
+}
+  const allowedTypes = [ToggleButton, ToggleOn, ToggleOff]
 
 function App() {
   return (
-    <div>
-      <Toggle>
-        <ToggleOn>The button is on</ToggleOn>
-        <ToggleOff>The button is off</ToggleOff>
-        <ToggleButton />
-      </Toggle>
-    </div>
+      <div>
+        <Toggle>
+          <ToggleOn>The button is on</ToggleOn>
+          <ToggleOff>The button is off</ToggleOff>
+          <span>Hello</span>
+          <ToggleButton />
+        </Toggle>
+      </div>
   )
 }
 
